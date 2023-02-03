e70ab7256ea15e717ae1c3496e08c1748e9a1f6c
Alexandre Duarte <alexteloduarte@gmail.com>
2023-02-03 21:59:24 +0000

    Enemy rotates towards player

--- Assets/Code/Enemy.cs
+++ Assets/Code/Enemy.cs
@@ -6,12 +6,24 @@ public class Enemy : MonoBehaviour
 {
     public Rigidbody2D rb;
     public Transform position;
+    public Transform playerPosition;
 
     public float speed = 5f;
 
     private void Start()
     {
-        Debug.Log(position.rotation.eulerAngles.z);
+        Debug.Log(position.position - playerPosition.position);
+
+        Vector3 targ = playerPosition.position;
+        targ.z = 0f;
+
+        Vector3 objectPos = transform.position;
+        targ.x = targ.x - objectPos.x;
+        targ.y = targ.y - objectPos.y;
+
+        float angle = Mathf.Atan2(targ.y, targ.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+
         rb.velocity = new Vector2(Mathf.Cos(position.rotation.eulerAngles.z * Mathf.Deg2Rad + Mathf.PI / 2),
                       Mathf.Sin(position.rotation.eulerAngles.z * Mathf.Deg2Rad + Mathf.PI / 2)) * speed;
 
