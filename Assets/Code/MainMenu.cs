d59548836df6eb411cf5abd7e11f7101f721fb0d
Rómulo Kaidussis <romulokaidussis@tecnico.ulisboa.pt>
2023-02-03 22:36:36 +0000

    Main Menu completed

---
+++ Assets/Code/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame() {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex)
+        Debug.Log("START GAME!");
+    }
+
+    public void QuitGame() {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
