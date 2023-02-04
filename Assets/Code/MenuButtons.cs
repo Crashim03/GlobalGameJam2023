c863ee46237aae2db601a0063f20d30d3684653d
Alexandre Duarte <alexteloduarte@gmail.com>
2023-02-04 21:48:50 +0000

    Menu fix

--- Assets/Code/MenuButtons.cs
+++ Assets/Code/MenuButtons.cs
@@ -5,22 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
-    public void PlayGame() {
+    public void PlayGame()
+    {
         SceneManager.LoadScene("MainScene");
         Debug.Log("START GAME!");
     }
 
-    public void ReturnMainMenu() {
-        //SceneManager.LoadScene(0) 
+    public void ReturnMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
         Debug.Log("RETURN TO MAIN MENU!");
     }
 
-    public void Retry() {
-        //SceneManager.LoadScene(1)
-        Debug.Log("RESTART GAME!"); 
+    public void Retry()
+    {
+        SceneManager.LoadScene("MainScene");
+        Debug.Log("RESTART GAME!");
     }
 
-    public void QuitGame() {
+    public void QuitGame()
+    {
         Debug.Log("QUIT!");
         Application.Quit();
     }
