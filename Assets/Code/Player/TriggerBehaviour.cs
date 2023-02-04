8639000680642796590c8fb8f0c43b57f3a3ad4c
Scarface1809 <joao.miguel.santos1809@gmail.com>
2023-02-04 12:31:09 +0000

    Player

---
+++ Assets/Code/Player/TriggerBehaviour.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBehaviour : MonoBehaviour
+{
+    private bool can_kill = false;
+    private GameObject enemy;
+
+    public void KillEnemy()
+    {
+        if(can_kill)
+        {
+            Destroy(enemy);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        can_kill = true;
+        enemy = other.gameObject;
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        can_kill = false;
+    }
+}
